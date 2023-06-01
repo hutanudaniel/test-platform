fb2db8e8533ceaac243b0d18458a4fb49991c1bc
hutanudaniel <daniel.hutanu@student.tuiasi.ro>
2023-06-01 18:40:24 +0300

    primul commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import MyAccount from './components/MyAccount/MyAccount';
+
+function App() {
+
+
+
+  const nume = "Hutanu Daniel";
+
+
+  return (
+    <div className='main'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="my-account" element={<MyAccount />} />
+          {/* <Route path="list" element={<CarList />} />
+          <Route path="add" element={<AddCar />} /> */}
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

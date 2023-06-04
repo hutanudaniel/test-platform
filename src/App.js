2d8d208c8464d3872d9524b19f8fcf094c42a9e5
agapealex <agapee.alex@yahoo.com>
2023-06-04 15:24:26 +0300

    register, verification code, sign in

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Home from './components/Home/Home';
 import MyAccount from './components/MyAccount/MyAccount';
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import awsExports from './aws-exports';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { createTodo } from './mutations';
 import { listTodos } from './queries';
+import Register from './components/Register/register';
+import SignIn from './components/Signin/signin';
+import VerificationPage from './components/VerificationPage/verificationPage';
 
 const todo = { name: 'My Todo', description: 'This is my todo' };
 
@@ -24,21 +27,22 @@ const todo = { name: 'My Todo', description: 'This is my todo' };
 
 function App() {
 
+  const [date, setDate] = useState(null);
+
   Amplify.configure(awsExports);
 
   useEffect(() => {
 
     const func = async () => {
-      const resp = await API.graphql(graphqlOperation(listTodos)).then(
+      await API.graphql(graphqlOperation(listTodos)).then(
         response => {
           const todos = response.data.listTodos.items;
+          setDate(todos);
           console.log('Lista todo-urilor:', todos);
       })
         .catch(error => {
           console.log('Error creating todo:', error);
         });
-
-      console.log(resp, "lista meaa")
     }
 
 
@@ -49,6 +53,9 @@ function App() {
 
   const nume = "Hutanu Daniel";
   // o modificare
+
+  console.log(date, "lista meaa")
+
 
   return (
     <div className='main'>
@@ -62,6 +69,10 @@ function App() {
 
         </Routes>
       </BrowserRouter>
+      <Register/>
+      <VerificationPage/>
+      ---------------------------
+      <SignIn/>
     </div>
   );
 }

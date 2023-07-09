b35b8e22a51138bdd9207a98de85377d09c4fbf9
agapealex <agapee.alex@yahoo.com>
2023-07-10 01:30:04 +0300

    add home, contact, deconectare

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { quiz } from '../../quiz';
-import Quiz from 'react-quiz-component';
-import './contact.css'
+import { quiz } from "../../quiz";
+import Quiz from "react-quiz-component";
+import "./contact.css";
 
 function Contact() {
-    return(
-        <div className="quiz" >
-            Contact
-        </div>
-    )
+  return (
+    <div className="quiz">
+    <div className="container">
+      <h1 className="title">Contactează-ne</h1>
+      <div className="contact-info">
+        <p className="contact-detail">
+          <span className="detail-label">Telefon:</span> 123-456-789
+        </p>
+        <p className="contact-detail">
+          <span className="detail-label">Email:</span> contact@example.com
+        </p>
+        <p className="contact-detail">
+          <span className="detail-label">Adresă:</span> Strada Exemplu, Nr. 123, Orașul Tau, 12345
+        </p>
+      </div>
+    </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,62 @@
 import React from "react";
+import "./Home.css";
+import { BrowserView, isMobile } from "react-device-detect";
+import { Auth } from "aws-amplify";
+import Button from "@mui/material/Button";
+
+
 
 function Home() {
-    return(
-        <div>
-            Home
-        </div>
-    )
+  const handleSignout = async (event) => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <img src="presentation.png" alt="Imagine" />
+      <div className="description">
+        <p>Pași pentru a ajunge la finalizarea cu succes a unui test:</p>
+        <ol>
+          <li>
+            În funcție de devisul folosit aveți în partea de jos sau sus meniul
+            aplicației;
+          </li>
+          <li>Parcurgeți mediul de învățare aflat în butonul "Lecții";</li>
+          <li>
+            În butonul "Teste" se regăsesc testele, de unde vă alegeți materia;
+          </li>
+          <li>
+            După ce ați ales testul, aveți doar o singură variantă de răspuns
+            corect;
+          </li>
+          <li>
+            După terminarea testului veți primi scorul în funcție de performanță
+            împreună cu variantele de răspuns corect și explicația întrebării.
+          </li>
+        </ol>
+        <p>Mult succes!</p>
+        <br/>
+        {isMobile && (
+          <Button
+            sx={{
+              marginLeft: "auto",
+              marginRight: "10px",
+              backgroundColor: "white",
+              color: "black",
+            }}
+            variant="contained"
+            onClick={handleSignout}
+          >
+            Deconectare
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
